4030c0c40ef7f514a3548f294f86b2d65840d9d6
Eric Mink <eric@mink.li>
2017-12-24 14:14:53 +0100

    added pubkey.gpg with only the relevant key to accept

--- autorun_StickMagic.bat
+++ autorun_StickMagic.bat
@@ -31,7 +31,7 @@ if defined debug (echo "verifying pgp signature...")
 ::disable any local variable called errorlevel
 set "errorlevel="
 ::verify the signature
-gpg --status-fd 1 --verify "Q:\.stickmagic.sig" 2>nul
+gpg --no-default-keyring --keyring "C:\Users\Eric\Desktop\autorun_stickmagic\pubkey.gpg" --status-fd 1 --verify "Q:\.stickmagic.sig" 2>nul
 if %ERRORLEVEL% NEQ 0 (
 	if %ERRORLEVEL% == 1 (
 		if defined debug (echo "BAD SIGNATURE.")
